0a5cdbd74eeceb6e2058ce62382417440588d952
MbusyaNgesa <theengesa@gmail.com>
2024-12-16 13:46:11 +0300

    Signup Endpoint

--- server/server.js
+++ server/server.js
@@ -9,13 +9,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Eventlify");
-});
+// app.get("/", (req, res) => {
+//   res.send("Eventlify");
+// });
+
+app.use(express.json());
+app.use("/api/auth", authroutes);
 
 app.listen(PORT, () => {
   connectDB();
   console.log("Server Running");
 });
-
-app.use("/api/auth", authroutes);
